cf7db75e6a6904c8f7c0af96c3f0cad76ec7815a
bryan <bryan@localhost.localdomain>
2017-10-03 16:51:01 -0500

    rendering db query

--- app/controllers/game_controller.rb
+++ app/controllers/game_controller.rb
@@ -1,4 +1,8 @@
 class GameController < ApplicationController
   def index
+    @season = ActiveRecord::Base.connection.execute(
+      "select game_id,scorehome,scoreaway,name,home from games
+      inner join homegames on games.id=homegames.game_id
+      inner join teams on teams.id=homegames.team_id")
   end
 end
